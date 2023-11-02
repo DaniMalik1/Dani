434f8376742262dd4b6617ba1e51f0b67f4f4a2a
Dani Malik <112042251+DaniMalik1@users.noreply.github.com>
2023-11-02 11:59:17 +0500

    Update Dani.py

--- Dani.py
+++ Dani.py
@@ -1,8 +1,3 @@
 import os
 os.system('git pull')
-print('Trial Finished')
-print('Ab trial k liye na aana')
-print('jis ney paid buy krna just wohi aana')
-print('Jazak Allah')
-print('Allah Hafiz')
-print('Contact 03485738314')
+import Anjan
